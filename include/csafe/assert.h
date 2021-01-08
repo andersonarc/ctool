2a9f52fdb669e24f6d1f8f8d5fd7afda71a0b49e
andersonarc <e.andersonarc@gmail.com>
2021-01-08 18:33:24 +0000

    assert function instead of macro, testing

--- include/csafe/assert.h
+++ include/csafe/assert.h
@@ -10,55 +10,64 @@
 #define CSAFE_ASSERT_H
 
     /* includes */
-#include <stdlib.h> /* exit() */
-#include <stdio.h>  /* print, stdout, stderr */
-#include <errno.h>  /* errno */
-#include <string.h> /* strerror() */
+#include <stdlib.h>  /* exit() */
+#include <stdio.h>   /* print, stdout, stderr */
+#include <errno.h>   /* errno */
+#include <string.h>  /* strerror() */
+#include <stdbool.h> /* bool */
+#include <stdarg.h>  /* vararg */
+
+    /* functions */
+/**
+ * @brief custom assertion function, for internal use only
+ */
+void _csafe_assert_impl(bool condition, char* message, ...);
 
     /* defines */
 /**
- * @brief generic assertion macro
+ * @brief csafe assertion
  */
-#define _generic_assert(condition, message, ...) \
-    if (condition) {                             \
-        fprintf(stderr, "csafe: assert failed: " message "\n", __VA_ARGS__); \
-        fprintf(stdout, "csafe: assert failed: " message "\n", __VA_ARGS__); \
-        exit(EXIT_FAILURE);                                             \
-    }
+#ifdef DEBUG
+    #define csafe_assert(condition, message, ...) _csafe_assert_impl(condition, message, __VA_ARGS__)
+#else
+    #define csafe_assert(condition, message, ...)
+#endif /* DEBUG /*
 
 /**
- * @brief errno format string
+ * @brief header-local message formatter
  */
-#define _ERRNO_POSTFIX "errno is %d (%s)", errno, strerror(errno)
-        
+#define __fmt(message) ("csafe: assert failed: " message "\n")
+
 /**
  * @brief asserts that variable is not null
  */
-#define assert_not_null(variable) _generic_assert(variable == NULL, "variable '" #variable "'" " is null, expected not null; " _ERRNO_POSTFIX);
+#define assert_not_null(variable)                       csafe_assert(variable == NULL, __fmt("variable '" #variable "'" " is null, expected not null;"));
 
 /**
  * @brief asserts that function result is not null
  */
-#define assert_not_null_function(function_call) _generic_assert(function_call == NULL, "function " #function_call " returned null, expected not null; " _ERRNO_POSTFIX);
+#define assert_not_null_function(function_call)         csafe_assert(function_call == NULL, __fmt("function " #function_call " returned null, expected not null;"));
 
 /**
  * @brief asserts that function result is not null and sets variable to the result
  */
-#define assert_not_null_function_set(expression, ...) _generic_assert((expression) == NULL, "expression (" #expression ") returned null, expected not null; " _ERRNO_POSTFIX);
+#define assert_not_null_function_set(expression, ...)   csafe_assert((expression) == NULL, __fmt("expression (" #expression ") returned null, expected not null;"));
 
 /**
  * @brief asserts that generic condition is true
  */
-#define assert_true(condition) _generic_assert(!condition, "condition (" #condition ")" " is false, expected true; " _ERRNO_POSTFIX);
+#define assert_true(condition)                          csafe_assert(!condition, __fmt("condition (" #condition ")" " is false, expected true;"));
 
 /**
  * @brief asserts that generic condition is false
  */
-#define assert_false(condition) _generic_assert(condition, "condition (" #condition ")" " is true, expected false; " _ERRNO_POSTFIX);
+#define assert_false(condition)                         csafe_assert(condition, __fmt("condition (" #condition ")" " is true, expected false;"));
 
 /**
  * @brief asserts that errno is zero
  */
-#define assert_errno() _generic_assert_format(errno != 0, "errno is %d (%s), expected 0", errno, strerror(errno));
+#define assert_errno()                                  csafe_assert(errno != 0, __fmt("errno is not equal to 0"));
+
+#undef _csfm /* undefine header-local macro */
 
 #endif /* CSAFE_ASSERT_H */
