98c69036aa0c8736d2f8e2b3591e8fd45b15e0a4
andersonarc <e.andersonarc@gmail.com>
2021-02-07 00:59:32 +0000

    fix formatted logging

--- include/csafe/log.h
+++ include/csafe/log.h
@@ -1,94 +1,70 @@
 /**
- * @file log.h
+ * @file logf.h
  * @author andersonarc (e.andersonarc@gmail.com)
- * @brief csafe logging
- * @version 0.1
+ * @brief csafe formatted logging
+ * @version 0.2
  * @date 2021-02-07
  */
     /* header guard */
-#ifndef CSAFE_LOG_H
-#define CSAFE_LOG_H
+#ifndef CSAFE_LOGF_H
+#define CSAFE_LOGF_H
 
     /* defines */
 /**
- * @brief log error with custom suffix and prefix
- * 
- * @param message message format string
- * @param function caller function name
- * @param suffix letter suffix
+ * @brief message with custom suffix and prefix
  */
-#define loge_sp(message, function, suffix, prefix, ...) _csafe_log_error(prefix "[" suffix "] " function ": " message, __VA_ARGS__)
+#define log_sp(message, function, suffix, prefix) _csafe_log_message(prefix "[" suffix "] " function ": " message)
+/**
+ * @brief message with custom suffix
+ */
+#define log_s(message, function, suffix) log_sp(message, function, suffix, prefix)
+
 
 /**
- * @brief log message with custom suffix and prefix
- * 
- * @param message message format string
- * @param function caller function name
- * @param suffix letter suffix
+ * @brief error with custom suffix and prefix
  */
-#define log_sp(message, function, suffix, prefix, ...) _csafe_log_message(prefix "[" suffix "] " function ": " message, __VA_ARGS__)
+#define loge_sp(message, function, suffix, prefix) _csafe_log_error(prefix "[" suffix "] " function ": " message)
+/**
+ * @brief error message with custom suffix
+ */
+#define loge_s(message, function, suffix) loge_sp(message, function, suffix, prefix)
+
 
 /**
- * @brief log with custom suffix
- * 
- * @param message message format string
- * @param function caller function name
- * @param suffix letter suffix
- */
-#define log_s(message, function, suffix, ...) log_sp(message, function, suffix, prefix, __VA_ARGS__)
-
-/**
- * @brief log error with custom suffix
- * 
- * @param message message format string
- * @param function caller function name
- * @param suffix letter suffix
- */
-#define loge_s(message, function, suffix, ...) loge_sp(message, function, suffix, prefix, __VA_ARGS__)
-
-/**
- * @brief log debug
- * 
- * @param message message format string
- * @param function caller function name
+ * @brief debug message
  */
 #ifdef DEBUG
-    #define logd(message, function, ...) log_s(message, function, "D", __VA_ARGS__)
+    #define logd(message, function) log_s(message, function, "D")
 #else
-    #define logd(message, function, ...)
+    #define logd(message, function)
 #endif /* DEBUG */
 
-/**
- * @brief log info
- * 
- * @param message message format string
- * @param function caller function name
- */
-#define logi(message, function, ...) log_s(message, function, "I", __VA_ARGS__)
 
 /**
- * @brief log warning
- * 
- * @param message message format string
- * @param function caller function name
+ * @brief info
  */
-#define logw(message, function, ...) loge_sp(message, function, "W", "!", __VA_ARGS__)
+#define logi(message, function) log_s(message, function, "I")
+
 
 /**
- * @brief log error
- * 
- * @param message message format string
- * @param function caller function name
+ * @brief warning
  */
-#define loge(message, function, ...) loge_sp(message, function, "E", "!!!", __VA_ARGS__)
+#define logw(message, function) loge_sp(message, function, "W", "!")
+
 
 /**
- * @brief log fatal error
- * 
- * @param message message format string
- * @param function caller function name
+ * @brief error
  */
-#define logef(message, function, ...) _csafe_exit_fatal("!!!!! [FATAL] " function ": " message, __VA_ARGS__)
+#define loge(message, function) loge_sp(message, function, "E", "!!!")
+
+
+/**
+ * @brief fatal error
+ */
+#define logfe(message, function) _csafe_log_fatal("!!!!! [FATAL] " function ": " message)
+
+
+
 
     /* functions */
 /**
@@ -96,20 +72,20 @@
  * 
  * @param message format string
  */
-void _csafe_log_message(char* message, ...);
+void _csafe_log_message(char* message);
 
 /**
  * @brief print an error, for internal use only
  * 
  * @param message format string
  */
-void _csafe_log_error(char* message, ...);
+void _csafe_log_error(char* message);
 
 /**
  * @brief print a message and exit, for internal use only
  * 
  * @param message format string
  */
-void _csafe_exit_fatal(char* message, ...);
+void _csafe_log_fatal(char* message);
 
 #endif /* CSAFE_LOG_H */
--- src/log.c
+++ src/log.c
@@ -2,7 +2,7 @@
  * @file log.c
  * @author andersonarc (e.andersonarc@gmail.com)
  * @brief csafe logging implementation
- * @version 0.1
+ * @version 0.2
  * @date 2021-02-06
  */
     /* includes */
@@ -11,42 +11,71 @@
 #include <stdio.h>     /* print, stdout, stderr */
 #include <stdarg.h>    /* vararg */
 
-    /* functions */
+    /* defines */
 /**
  * @brief print a message, for internal use only
  * 
+ * @param message string
+ */
+#define _csafe_log_message(message) puts(message)
+
+    /* functions */
+/**
+ * @brief print a formatted message, for internal use only
+ * 
  * @param message format string
  */
-void _csafe_log_message(char* message, ...) {
+void _csafe_logf_message(char* message, ...) {
     va_list format;
     va_start(format, message);
     vfprintf(stderr, message, format);
     va_end(format);
+}
+
+/**
+ * @brief print a formatted error, for internal use only
+ * 
+ * @param message format string
+ */
+void _csafe_logf_error(char* message, ...) {
+    va_list format;
+    va_start(format, message);
+    vfprintf(stderr, message, format);
+    vfprintf(stdout, message, format);
+    va_end(format);
+}
+
+/**
+ * @brief print a formatted error and exit, for internal use only
+ * 
+ * @param message format string
+ */
+void _csafe_logf_fatal(char* message, ...) {
+    va_list format;
+    va_start(format, message);
+    vfprintf(stderr, message, format);
+    vfprintf(stdout, message, format);
+    va_end(format);
+    exit(1);
 }
 
 /**
  * @brief print an error, for internal use only
  * 
- * @param message format string
+ * @param message string
  */
-void _csafe_log_error(char* message, ...) {
-    va_list format;
-    va_start(format, message);
-    vfprintf(stderr, message, format);
-    vfprintf(stdout, message, format);
-    va_end(format);
+void _csafe_log_error(char* message) {
+    fputs(message, stderr);
+    fputs(message, stdout);
 }
 
 /**
  * @brief print a message and exit, for internal use only
  * 
- * @param message format string
+ * @param message string
  */
-void _csafe_exit_fatal(char* message, ...) {
-    va_list format;
-    va_start(format, message);
-    vfprintf(stderr, message, format);
-    vfprintf(stdout, message, format);
-    va_end(format);
+void _csafe_log_fatal(char* message) {
+    fputs(message, stderr);
+    fputs(message, stdout);
     exit(1);
 }
