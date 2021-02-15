f363e796ebc66fffeeb60e8c1d7c975ad4f0a120
andersonarc <e.andersonarc@gmail.com>
2021-02-15 20:23:45 +0000

    add status, rename meson dependency

---
+++ include/csafe/status.h
@@ -0,0 +1,26 @@
+/**
+ * @file status.h
+ * @author andersonarc (e.andersonarc@gmail.com)
+ * @brief return status for functions
+ * @version 0.1
+ * @date 2021-02-15
+ */
+    /* header guard */
+#ifndef CSAFE_STATUS_H
+#define CSAFE_STATUS_H
+
+    /* typedefs */
+/**
+ * @brief status structure
+**/
+typedef enum status_t {
+    ST_OK, ST_FAIL, ST_FILE_FAIL, ST_ALLOC_FAIL, ST_NET_FAIL, ST_BAD_ARG
+} status_t;
+
+    /* defines */
+/**
+ * @brief condition check and return with status
+**/
+#define return_if(condition, status) if (condition) { return status; }
+
+#endif /* CSAFE_STATUS_H */
