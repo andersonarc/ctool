08faf8e286dbd3a855a0de20e4554c51810eefb4
andersonarc <e.andersonarc@gmail.com>
2021-08-28 22:16:47 +0000

    use macro_concatenate

--- include/ctool/type/_internal.h
+++ include/ctool/type/_internal.h
@@ -10,6 +10,9 @@
 #ifndef CTOOL_TYPE_INTERNAL_H
 #define CTOOL_TYPE_INTERNAL_H
 
+    /* includes */
+#include "ctool/macro.h" /* macro utilities */
+
     /* defines */
 /**
  * Merges a base name (e.g. array, list),
@@ -20,7 +23,7 @@
  * @param[in] type     The generic type
  * @param[in] function The function name
  */
-#define _ctool_generic_function(base, type, function) type##_##base##_##function
+#define _ctool_generic_function(base, type, function) macro_concatenate(macro_concatenate(macro_concatenate(macro_concatenate(type, _), base), _), function)
 
 /**
  * Merges a base name (e.g. array, list) and
@@ -30,6 +33,6 @@
  * @param[in] base The base type
  * @param[in] type The generic type
  */
-#define _ctool_generic_type(base, type) type##_##base##_t
+#define _ctool_generic_type(base, type) macro_concatenate(macro_concatenate(macro_concatenate(type, _), base), _t)
 
 #endif /* CTOOL_TYPE_INTERNAL_H */
